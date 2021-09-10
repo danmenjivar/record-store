fe83b092420435dd6c6c3c0690e511ea4952e09e
Dan Menjivar <contactdanmenjivar@gmail.com>
2021-09-10 10:43:23 -0700

    Add view for artist detail

--- controllers/artistController.js
+++ controllers/artistController.js
@@ -1,4 +1,6 @@
 var Artist = require('../models/artist');
+var Album = require('../models/album');
+var async = require('async');
 
 // Display list of all Artists
 exports.artist_list = function (req, res, next) {
@@ -15,8 +17,37 @@ exports.artist_list = function (req, res, next) {
 };
 
 // Display detail page for a specific artist.
-exports.artist_detail = function (req, res) {
-    res.send('NOT IMPLEMENTED: artist detail: ' + req.params.id);
+exports.artist_detail = function (req, res, next) {
+    async.parallel(
+        {
+            artist: function (callback) {
+                Artist.findById(req.params.id).exec(callback);
+            },
+            artist_albums: function (callback) {
+                Album.find({ artist: req.params.id }, 'title description').exec(
+                    callback
+                );
+            },
+        },
+        function (err, results) {
+            if (err) {
+                // API goes wrong
+                return next(err);
+            }
+            if (results.artist == null) {
+                // No results
+                var err = new Error('Artist not found');
+                err.status = 404;
+                return next(err);
+            }
+            // Success, render
+            res.render('artist_detail', {
+                title: 'Artist Detail',
+                artist: results.artist,
+                artist_albums: results.artist_albums,
+            });
+        }
+    );
 };
 
 // Display artist create form on GET.
